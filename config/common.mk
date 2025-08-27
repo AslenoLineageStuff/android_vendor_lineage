keepQASSA/vendor_qassa

Custom build version 2025-05-05
* https://review.lineageos.org/q/topic:%22Q_asb_2025-05%22

Signed-off-by: Lin Jian <vrayananda@yahoo.com>

config/common.mk
@@ -1,7 +1,7 @@
 PRODUCT_BRAND ?= QASSA
 
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.kasa.custom_version=2024-10-01
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.kasa.custom_version=2025-05-05
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 

AslenoLineageStuff/android_vendor_lineage

config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2024-10-01
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2025-05-05
 
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)